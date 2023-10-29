912864a5182d076e052ac5388b877fe257b99e89
AChangAZha <80208659+AChangAZha@users.noreply.github.com>
2023-10-29 23:05:31 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import axios from 'axios'
 import router from './router'
 
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 const app = createApp(App);
 app.use(router)
 app.provide('axios', axios);
